96d6c8d0e4f0cbf5d24c51d64c71793215528869
uguumur <uguumur.oldokh@gmail.com>
2024-07-21 17:24:11 -0500

    lesson 6 last parts

--- js/lesson6.js
+++ js/lesson6.js
@@ -25,23 +25,67 @@ Array.prototype.bubbleSort = function () {
 
 console.log([6, 4, 0, 3, -2, 1].bubbleSort());
 
-const Person = {
-  initialize(name) {
-    this.name = name;
-  },
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+Person.prototype.greeting = function () {
+  console.log(
+    `Greetings, my name is ${this.name} and I am ${this.age} years old.`
+  );
 };
 
-const Teacher = Object.create(Person);
-
-Teacher.create = function (name) {
-  const teacher = Object.create(this);
-  teacher.initialize(name);
-  return teacher;
+Person.prototype.salute = function () {
+  console.log(
+    `Good morning!, and in case I don't see you, good afternoon, good evening and good night!`
+  );
 };
 
-Teacher.teach = function (subject) {
+function Student(name, age, major) {
+  Person.call(this, name, age);
+  this.major = major;
+}
+
+Student.prototype = Object.create(Person.prototype);
+Student.prototype.constructor = Student;
+
+function Teacher(name) {
+  this.name = name;
+}
+
+Teacher.prototype = Object.create(Person.prototype);
+Teacher.prototype.constructor = Teacher;
+
+Teacher.prototype.teach = function (subject) {
   console.log(`${this.name} is now teaching ${subject}`);
 };
 
-const teacher = Teacher.create("Grayson");
+Student.prototype.greeting = function () {
+  console.log(`Hey, my name is ${this.name} and I am studying ${this.major}.`);
+};
+
+function Professor(name, age, department) {
+  Person.call(this, name, age);
+  this.department = department;
+}
+
+Professor.prototype = Object.create(Person.prototype);
+Professor.prototype.constructor = Professor;
+
+Professor.prototype.greeting = function () {
+  console.log(
+    `Good day, my name is ${this.name} and I am in the ${this.department} department.`
+  );
+};
+
+const student2 = new Student("Grayson", 32, "Modern Web Application");
+student2.greeting();
+student2.salute();
+
+const professor2 = new Professor("Aruka", 28, "Database");
+professor2.greeting();
+professor2.salute();
+
+const teacher = new Teacher("Grayson");
 teacher.teach("Web Application");
